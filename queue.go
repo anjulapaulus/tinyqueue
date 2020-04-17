50b6b781f0276a5478591d0644b1a1654cb733f3
Anjula <anjula@Anjulas-MacBook-Pro.local>
2020-04-17 20:24:42 +0530

    Tiny Queue Implementation

--- queue.go
+++ queue.go
@@ -1,91 +1,88 @@
 package tinyqueue
 
 type Queue struct {
-	length 	int
-	ids 	[]interface{}
-	values 	[]Item
+	length 		int
+	data   		[]Item
 }
 
 type Item interface {
-	Less (Item) bool
+	Compare(Item) bool
 }
 
-func (q *Queue) Push (id interface{}, value Item){
-	q.ids = append(q.ids,id)
-	q.values = append(q.values,value)
-	q.length ++
-	pos := q.length -1
+func (q *Queue) Len () int{
+	return q.length
+}
 
+func (q *Queue) PeekMin () Item{
+	return q.data[0]
+}
+
+func (q *Queue) PeekMax () Item{
+	return q.data[q.length-1]
+}
+
+func (q *Queue) All() []Item{
+	return q.data
+}
+
+func (q *Queue) Push (item Item){
+	if item == nil{
+		return
+	}
+	q.data = append(q.data,item)
+	q.length++
+	q.up(q.length-1)
+}
+
+func (q *Queue) up (pos int){
+	data := q.data
+	item := data[pos]
 	for pos > 0 {
-		parent := (pos -1) >> 1
-		parentValue := q.values[parent]
-		if !q.values[pos].Less(parentValue) {
+		parent := (pos-1) >> 1
+		current := data[parent]
+		if !item.Compare(current){
 			break
 		}
-		q.ids[pos] = q.ids[parent]
-		q.values[pos] = parentValue
+		data[pos] = current
 		pos = parent
 	}
-	q.ids[pos] = id
-	q.values[pos] = value
+	data[pos] = item
 }
-func (q *Queue) Pop () interface{}{
-	if q.length == 0{
+
+func (q *Queue) Pop() Item {
+	if q.length == 0 {
 		return nil
 	}
-
-	top := q.ids[0]
+	top := q.data[0]
 	q.length--
-
-	if q.length > 0 {
-		q.ids[0] = q.ids[q.length]
-		q.values[0] = q.values[q.length]
-
-		id := q.ids[0]
-		value := q.values[0]
-		halfLength := q.length >> 1
-		pos := 0
-
-		for pos < halfLength {
-			left := (pos << 1) + 1
-			right := left + 1
-			bestIndex := q.ids[left]
-			bestValue := q.values[left]
-			rightValue := q.values[right]
-
-			if right < q.length && rightValue.Less(bestValue) {
-				left = right
-				bestIndex = q.ids[right]
-				bestValue = rightValue
-			}
-			if !bestValue.Less(value) {
-				break
-			}
-
-			q.ids[pos] = bestIndex
-			q.values[pos] = bestValue
-			pos = left
-		}
-		q.ids[pos] = id
-		q.values[pos] = value
+	if q.length > 0{
+		q.data[0] = q.data[q.length]
+		q.down(0)
 	}
+	q.data = q.data[:len(q.data)-1]
 	return top
 }
 
-func (q *Queue) Peek() interface{} {
-	if q.length == 0 {
-		return nil
-	}
-	return q.ids[0]
-}
+func (q *Queue) down (pos int){
+	data := q.data
+	halfLength := q.length >> 1
+	item := q.data[pos]
 
-func (q *Queue) PeekValue() Item{
-	if q.length == 0 {
-		return nil
-	}
-	return q.values[0]
-}
+	for pos < halfLength {
+		left := (pos << 1) + 1
+		best := q.data[left]
+		right := left + 1
 
-func (q *Queue) Len() int{
-	return q.length
+		if right < q.length && data[right].Compare(best) {
+			left = right
+			best = data[right]
+		}
+
+		if !best.Compare(item) {
+			break
+		}
+		data[pos] = best
+		pos = left
+	}
+	data[pos] = item
 }
--- queue_test.go
+++ queue_test.go
@@ -1,66 +1,80 @@
 package tinyqueue
 
 import (
-	"fmt"
 	"testing"
 )
 
-type floatValue float64
+type IntVal int64
 
-func (a floatValue) Less(b Item) bool {
-	return a >= b.(floatValue)
+func (a IntVal) Compare (b Item) bool{
+	return a <= b.(IntVal)
 }
 
 func TestQueue_Push(t *testing.T) {
-	var q Queue
-	q.Push(0, floatValue(1))
-	q.Push(1, floatValue(2))
-	 if q.Len() != 2{
-	 	t.Error("Push Function Failed")
-	 }
-}
 
-func BenchmarkQueue_Push(b *testing.B) {
-	var q Queue
-			for i := 0; i < b.N; i++ {
-				q.Push(0, floatValue(1))
-			}
-}
+	q := Queue{}
+	q.Push(IntVal(7))
+	q.Push(IntVal(5))
+	q.Push(IntVal(10))
 
-func TestQueue_Len(t *testing.T) {
-	var q Queue
-	q.Push(0, floatValue(1))
-	q.Push(1, floatValue(2))
-	if q.Len() != 2{
-		t.Error("Push Function Failed")
+	if q.Len() != 3{
+		t.Fatal("Push Test : Failed")
 	}
-}
 
-func TestQueue_Peek(t *testing.T) {
-	var q Queue
-	q.Push(0, floatValue(1))
-	q.Push(1, floatValue(2))
-
-	if q.Peek() != 1{
-		t.Error("Peek Function Failed")
-	}
-}
-
-func TestQueue_PeekValue(t *testing.T) {
-	var q Queue
-	q.Push(0, floatValue(1))
-	q.Push(1, floatValue(2))
-
-	val := q.PeekValue()
-	fmt.Println(val)
 }
 
 func TestQueue_Pop(t *testing.T) {
-	var q Queue
-	q.Push(0, floatValue(1))
-	q.Push(1, floatValue(2))
+	q := Queue{}
+	q.Push(IntVal(7))
+	q.Push(IntVal(5))
+	q.Push(IntVal(10))
 
-	if q.Pop() != 1{
-		t.Error("Pop function failed")
+	if q.Pop() != IntVal(5) {
+		t.Fatal("Pop Test : Failed")
+	}
+}
+
+func TestQueue_Len(t *testing.T) {
+	q := Queue{}
+	q.Push(IntVal(7))
+	q.Push(IntVal(5))
+	q.Push(IntVal(10))
+
+	if q.Len() != 3{
+		t.Fatal("Len Test: Failed")
+	}
+}
+
+func TestQueue_All(t *testing.T) {
+	q := Queue{}
+	q.Push(IntVal(7))
+	q.Push(IntVal(5))
+	q.Push(IntVal(10))
+
+	array := q.All()
+	if array[0] != IntVal(5) && array[1] != IntVal(7) && array[2] != IntVal(10){
+		t.Fatal("All Test: Failed")
+	}
+}
+
+func TestQueue_PeekMin(t *testing.T) {
+	q := Queue{}
+	q.Push(IntVal(7))
+	q.Push(IntVal(5))
+	q.Push(IntVal(10))
+
+	if q.PeekMin() != IntVal(5) {
+		t.Fatal("PeekMin Test: Failed")
+	}
+}
+
+func TestQueue_PeekMax(t *testing.T) {
+	q := Queue{}
+	q.Push(IntVal(7))
+	q.Push(IntVal(5))
+	q.Push(IntVal(10))
+
+	if q.PeekMax() != IntVal(10) {
+		t.Fatal("PeekMax Test: Failed")
 	}
 }
